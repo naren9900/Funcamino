c04e4fd1ed3b182a0cce55bb9f1bf532fe3739e9
Naren Rios Doza <narenriosdoza@gmail.com>
2026-02-19 07:23:30 -0500

    Creacion de paises y diseño del panel de usuarios

--- fundacionCamino/urls.py
+++ fundacionCamino/urls.py
@@ -21,4 +21,5 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(('educate.urls', 'educate'), namespace='educate')),
+    path('', include(('configuracion.urls', 'configuracion'), namespace='configuracion')),
 ]
