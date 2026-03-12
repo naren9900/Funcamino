972458b3c0a64287a2742ec9595614a6e32f3819
Naren Rios Doza <narenriosdoza@gmail.com>
2026-03-11 20:10:15 -0500

    eliminar Archivos

--- context/educate/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-
-app_name = 'educate'
-
-urlpatterns = [
-    path('', views.index, name='index'),
-]
--- core/models.py
+++
@@ -1,6 +0,0 @@
-from .configuration.continent import Continent
-from .configuration.country import Country
-from .configuration.department import Department
-from .configuration.municipality import Municipality
-
-__all__ = ['Continent','Country','Department','Municipality']
